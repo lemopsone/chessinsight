e622a59d87049eddbbce310fe9d56e2f41c6dd1c
lemopsone <maxim@varchenko.ru>
2026-01-27 14:29:50 +0300

    fix(cli): удаление модуля jpa из зависимостей
    используем REST API, нет нужды в подключении к БД

--- cli/src/main/java/ru/chessinsight/infrastructure/cli/CliApplication.java
+++ cli/src/main/java/ru/chessinsight/infrastructure/cli/CliApplication.java
@@ -2,10 +2,17 @@ package ru.chessinsight.infrastructure.cli;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication(scanBasePackages = "ru.chessinsight")
+@SpringBootApplication(
+    scanBasePackages = "ru.chessinsight",
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class CliApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(CliApplication.class)
